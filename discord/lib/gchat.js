c59605256f47bbf52b69161554b650fbbe2b3de9
meishu <theme97@gmail.com>
2016-08-17 15:55:51 -0700

    Move text conversion operations to util

--- discord/lib/gchat.js
+++ discord/lib/gchat.js
@@ -2,16 +2,6 @@
 
 const U = require('./util');
 
-// helpers
-function replaceAll(string, search, replace) {
-  return string.replace(new RegExp(U.escapeRegExp(search), 'gi'), replace);
-}
-
-function getName(server, user) {
-  const details = server.detailsOf(user);
-  return (details && details.nick) || (user && user.username) || '(???)';
-}
-
 // main
 module.exports = function gchatModule(app, config) {
   if (!config.channels.gchat) {
@@ -22,58 +12,6 @@ module.exports = function gchatModule(app, config) {
   //const {bot, ipc} = app;
   const bot = app.bot;
   const ipc = app.ipc;
-
-  ipc.on('chat', (author, message) => {
-    //const {server, channel} = this;
-    const server = this.server;
-    const channel = this.channel;
-    if (!server || !channel) return;
-
-    // convert HTML to text
-    message = U.unHtml(message);
-
-    // convert @mention
-    // 1 - nicknames
-    for (let user of server.members) {
-      const d = server.detailsOf(user);
-      if (d.nick != null) {
-        message = replaceAll(message, '@' + d.nick, user.mention());
-      }
-    }
-
-    // 2 - usernames
-    for (let user of server.members) {
-      message = replaceAll(message, '@' + user.username, user.mention());
-    }
-
-    // convert #channel
-    for (let ch of server.channels) {
-      if (ch.type !== 'text') continue;
-      message = replaceAll(message, '#' + ch.name, ch.mention());
-    }
-
-    // convert @role
-    for (let role of server.roles) {
-      message = replaceAll(message, '@' + role.name, role.mention());
-    }
-
-    // send
-    bot.sendMessage(channel, '[' + author + ']: ' + U.emojify(message));
-  });
-
-  ipc.on('guild', (motd, names) => {
-    if (!this.channel) return;
-    names.sort((a, b) => a.localeCompare(b));
-    bot.setChannelTopic(this.channel,
-      'Online: ' + names.join(', ') + ' // ' +
-      'MotD: ' + U.emojify(U.unHtml(motd))
-    );
-  });
-
-  ipc.on('sysmsg', (message) => {
-    if (!this.channel) return;
-    bot.sendMessage(this.channel, U.emojify(U.unHtml(message)));
-  });
 
   bot.on('ready', () => {
     const server = bot.servers.get('id', config['server-id']);
@@ -99,40 +37,38 @@ module.exports = function gchatModule(app, config) {
       return;
     }
 
-    this.server = server;
-    this.channel = channel;
     console.log('routing gchat to #%s (%s)', channel.name, channel.id);
-
     ipc.send('fetch');
-  });
 
-  bot.on('message', (message) => {
-    //const {server, channel} = this;
-    const server = this.server;
-    const channel = this.channel;
-    if (!message.channel.equals(channel)) return;
-    if (message.author.equals(bot.user)) return;
+    /*********
+     * hooks *
+     *********/
+    ipc.on('chat', (author, message) => {
+      // convert TERA HTML to Discord text
+      message = U.emojify(U.toDiscord(U.unHtml(message), server));
+      bot.sendMessage(channel, `[${author}]: ${message}`);
+    });
 
-    const str = U.unemojify(message.content)
-      // @user, @!user
-      .replace(/<@!?(\d+)>/g, (_, mention) => {
-        const m = server.members.get('id', mention);
-        return '@' + getName(server, m);
-      })
-      // #channel
-      .replace(/<#(\d+)>/g, (_, mention) => {
-        const m = server.channels.get('id', mention);
-        return '#' + ((m && m.name) || '(???)');
-      })
-      // @role
-      .replace(/<@&(\d+)>/g, (_, mention) => {
-        const m = server.roles.get('id', mention);
-        return '@' + ((m && m.name) || '(???)');
-      })
-      // :emoji:
-      .replace(/<:(\w+):(\d+)>/g, (_, mention) => ':' + mention + ':');
+    ipc.on('guild', (motd, names) => {
+      names.sort((a, b) => a.localeCompare(b));
+      bot.setChannelTopic(channel,
+        'Online: ' + names.join(', ') + ' // ' +
+        'MotD: ' + U.emojify(U.unHtml(motd))
+      );
+    });
 
-    const author = getName(server, message.author);
-    ipc.send('chat', author, str);
+    ipc.on('sysmsg', (message) => {
+      // don't convert mentions; highlights from TERA login message are abusable
+      bot.sendMessage(channel, U.emojify(U.unHtml(message)));
+    });
+
+    bot.on('message', (message) => {
+      if (!message.channel.equals(channel)) return;
+      if (message.author.equals(bot.user)) return;
+
+      const author = U.getName(server, message.author);
+      const str = U.unemojify(U.fromDiscord(message.content, server));
+      ipc.send('chat', author, str);
+    });
   });
 };
--- discord/lib/util/index.js
+++ discord/lib/util/index.js
@@ -49,10 +49,77 @@ const unemojify = (() => {
   };
 })();
 
+function replaceAll(string, search, replace) {
+  return string.replace(new RegExp(escapeRegExp(search), 'gi'), replace);
+}
+
+function getName(server, user) {
+  const details = server.detailsOf(user);
+  return (details && details.nick) || (user && user.username) || '(???)';
+}
+
+function toDiscord(message, server) {
+  // convert @mention
+  // 1 - nicknames
+  for (let user of server.members) {
+    const d = server.detailsOf(user);
+    if (d.nick != null) {
+      message = replaceAll(message, '@' + d.nick, user.mention());
+    }
+  }
+
+  // 2 - usernames
+  for (let user of server.members) {
+    message = replaceAll(message, '@' + user.username, user.mention());
+  }
+
+  // convert #channel
+  for (let ch of server.channels) {
+    if (ch.type !== 'text') continue;
+    message = replaceAll(message, '#' + ch.name, ch.mention());
+  }
+
+  // convert @role
+  for (let role of server.roles) {
+    message = replaceAll(message, '@' + role.name, role.mention());
+  }
+
+  // TODO convert :emoji:
+
+  // return
+  return message;
+}
+
+function fromDiscord(message, server) {
+  return (message
+    // @user, @!user
+    .replace(/<@!?(\d+)>/g, (_, mention) => {
+      const m = server.members.get('id', mention);
+      return '@' + getName(server, m);
+    })
+    // #channel
+    .replace(/<#(\d+)>/g, (_, mention) => {
+      const m = server.channels.get('id', mention);
+      return '#' + ((m && m.name) || '(???)');
+    })
+    // @role
+    .replace(/<@&(\d+)>/g, (_, mention) => {
+      const m = server.roles.get('id', mention);
+      return '@' + ((m && m.name) || '(???)');
+    })
+    // :emoji:
+    .replace(/<:(\w+):(\d+)>/g, (_, mention) => ':' + mention + ':')
+  );
+}
+
 // exports
 module.exports = {
   escapeRegExp: escapeRegExp,
   unHtml: unHtml,
   emojify: emojify,
   unemojify: unemojify,
+  replaceAll: replaceAll,
+  getName: getName,
+  toDiscord: toDiscord,
+  fromDiscord: fromDiscord,
 };
