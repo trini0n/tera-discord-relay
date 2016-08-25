ca7217d7c04914d4acc1e052f8582eb768836c59
meishu <theme97@gmail.com>
2016-08-24 20:59:36 -0700

    Fix entry module not doing anything

--- discord/lib/entry.js
+++ discord/lib/entry.js
@@ -1,12 +1,38 @@
 'use strict';
 
 module.exports = function entryModule(app, config) {
-  if (!config || !config.channels) return;
+  if (!config.channels['entry']) return;
 
-  app.bot.on('serverNewMember', (server, user) => {
-    const channel = config.channels['entry'];
-    if (channel) {
-      bot.sendMessage(channel, `@everyone please give $(user) a warm welcome!`);
+  const bot = app.bot;
+
+  bot.on('ready', () => {
+    const server = bot.servers.get('id', config['server-id']);
+    if (!server) {
+      console.error('server "%s" not found', config['server-id']);
+      console.error('servers:');
+      for (let s of bot.servers) {
+        console.error('- %s (%s)', s.name, s.id);
+      }
+      bot.logout();
+      return;
     }
+
+    const channel = server.channels.get('id', config.channels['entry']);
+    if (!channel || channel.type !== 'text') {
+      console.error('text channel "%s" not found', config.channels['entry']);
+      console.error('channels:');
+      for (let c of server.channels) {
+        if (c.type !== 'text') continue;
+        console.error('- #%s (%s)', c.name, c.id);
+      }
+      bot.logout();
+      return;
+    }
+
+    console.log('routing entry to #%s (%s)', channel.name, channel.id);
+
+    bot.on('serverNewMember', (server, user) => {
+      bot.sendMessage(channel, `@everyone please give ${user} a warm welcome!`);
+    });
   });
 };
