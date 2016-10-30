bd4d21c307188774c16a983c95cda9303c08cd60
meishu <theme97@gmail.com>
2016-10-30 01:26:37 -0700

    Use latest tera-proxy-game and move auth code into its own repo
    This also moves the package.json up a directory and makes it shared for both
    bots.
    
    To do: Convert TERA code to es6 and update Discord bot to discord.js v10.

--- tera/index.js
+++ tera/index.js
@@ -10,18 +10,150 @@ if (fn == null) {
 var config = JSON.parse(fs.readFileSync(fn, 'utf8'));
 
 // client
-var webClient = require('./lib/web');
-var gameClient = require('./lib/game');
+var webClient = require('tera-auth-ticket');
+var gameClient = require('tera-proxy-game');
 
+var describe = (function() {
+  var races = ['Human', 'High Elf', 'Aman', 'Castanic', 'Popori', 'Baraka'];
+  var genders = ['Male', 'Female'];
+
+  var classes = [
+    'Warrior', 'Lancer', 'Slayer', 'Berserker', 'Sorcerer', 'Archer',
+    'Priest', 'Mystic', 'Reaper', 'Gunner', 'Brawler', 'Ninja'
+  ];
+
+  return function describe(character) {
+    var description = '';
+
+    // race & gender
+    var race = races[character.race] || '?';
+    var gender = genders[character.gender] || '?';
+
+    if (character.race < 4) {
+      description += race + ' ' + gender;
+    } else {
+      if (character.race === 4 && character.gender === 1) {
+        description += 'Elin';
+      } else {
+        description += race;
+      }
+    }
+
+    // class
+    description += ' ' + (classes[character['class']] || '?') + ' / ';
+
+    // level
+    description += character.level;
+
+    // return
+    return description;
+  };
+})();
+
+// main
 var web = new webClient(config.email, config.pass);
 web.getLogin(function(err, data) {
   if (err) return;
 
-  var game = new gameClient(data.name, data.ticket, config.character);
-  game.connect({ host: config.host, port: config.port });
+  var connection = new gameClient.Connection();
+  var client = new gameClient.FakeClient(connection);
+  var srvConn = connection.connect(client, { host: config.host, port: config.port });
 
+  // set up core bot features
+  connection.dispatch.load('<core>', function coreModule(dispatch) {
+    // `connect` handler
+    client.on('connect', function onConnect() {
+      // version check
+      dispatch.toServer('cCheckVersion', {
+        version: [
+          { index: 0, value: 0x0004AE09 },
+          { index: 1, value: 0x0004ADAF },
+        ]
+      });
+
+      // authorization
+      dispatch.toServer('cLoginArbiter', {
+        unk1: 0,
+        unk2: 0,
+        unk3: 2,
+        unk4: 9901, // patch version
+        name: data.name,
+        ticket: new Buffer(data.ticket),
+      });
+    });
+
+    // get character list
+    dispatch.hook('sLoginAccountInfo', function() {
+      dispatch.toServer('cGetUserList');
+    });
+
+    dispatch.hook('sGetUserList', function(event) {
+      // parse character list
+      var characters = {};
+      for (var i = 0, len = event.characters.length; i < len; i++) {
+        var character = event.characters[i];
+        characters[character.name.toLowerCase()] = {
+          id: character.id,
+          description: character.name + ' [' + describe(character) + ']',
+        };
+      }
+
+      // find matching character
+      character = characters[config.character.toLowerCase()];
+      if (!character) {
+        console.error('[client] no character "' + config.character + '"');
+        console.error('[client] character list:');
+        for (var name in characters) {
+          character = characters[name];
+          console.error('- ' + character.description + ' (id: ' + character.id + ')');
+        }
+      } else {
+        console.log('[client] logging onto ' + character.description + ' (id: ' + character.id + ')');
+        dispatch.toServer('cSelectUser', {
+          id: character.id,
+          unk: 0,
+        });
+      }
+    });
+
+    // login sequence
+    dispatch.hook('sLoadTopo', function() {
+      dispatch.toServer('cLoadTopoFin');
+    });
+
+    // ping-pong
+    dispatch.hook('sPing', function() {
+      dispatch.toServer('cPong');
+    });
+
+    // terminate when connection ends
+    client.on('close', function onClose() {
+      process.exit();
+    });
+  });
+
+  // load modules
   for (var moduleName in config.modules) {
     var moduleConfig = config.modules[moduleName];
-    game.client.dispatch.load(moduleName, moduleConfig, module);
+    connection.dispatch.load('./app/' + moduleName, module, moduleConfig);
   }
+
+  // logging
+  srvConn.setTimeout(30 * 1000);
+
+  srvConn.on('connect', function onConnect() {
+    console.log('<connected to ' + srvConn.remoteAddress + ":" + srvConn.remotePort + '>');
+  });
+
+  srvConn.on('timeout', function onTimeout() {
+    console.log('<timeout>');
+  });
+
+  srvConn.on('close', function onClose() {
+    console.log('<disconnected>');
+  });
+
+  srvConn.on('error', function onError(err) {
+    console.warn(err);
+  });
 });
